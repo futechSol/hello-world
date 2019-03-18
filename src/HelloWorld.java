7545a398cd65b4023f70fb7d15e123168be36a66
Sudhakar <sbsudhakar55@gmail.com>
2019-03-18 12:12:30 +0530

    updated helloworld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,10 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		HelloWorld.test();
+	}
+	
+	public static void test() {
 		for (int i = 1; i <= 10; i++) {
 			System.out.println("Hello world... " + i);
 		}
